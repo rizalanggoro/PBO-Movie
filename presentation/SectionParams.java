23bc19e56ee92b8150947152b85c41d47d289add
Rizal Anggoro <gnoogler4@gmail.com>
2023-12-09 07:50:41 +0700

    0750

---
+++ presentation/SectionParams.java
@@ -0,0 +1,8 @@
+package presentation;
+
+import data.sources.SourceMovie;
+import domain.models.Movie;
+
+public class SectionParams {
+  public static Movie selectedMovie = SourceMovie.list.get(0);
+}
